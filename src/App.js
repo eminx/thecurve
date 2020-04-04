7c791c0f429300c0c22d402cfde67ce93fdeff8d
Emin Durak <emindurak@Emins-MacBook-Pro.local>
2020-04-04 18:29:36 +0530

    oh man what an app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Heading, Title, Subtitle } from 'bloomer';
 import './App.css';
+import 'bulma/css/bulma.css';
+
+import Drawer from './Drawer';
+
+const dataProvider = 'https://github.com/CSSEGISandData/COVID-19';
+const dataParser = 'https://github.com/pomber/covid19';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container style={{ paddingTop: 12 }} hasTextAlign="centered">
+        <Heading style={{ fontSize: 24, letterSpacing: -1 }}>
+          <b>the Curve</b>
+        </Heading>
+        <Subtitle isSize={6} style={{ marginBottom: 0 }}>
+          live data per country: related to Covid-19 diagnosis, recovery and
+          death <br />
+        </Subtitle>
+        <Drawer />
+        <Container style={{ padding: 48 }}>
+          <Subtitle isSize={6}>
+            data retrieved from <a href={dataProvider}>Johns Hopkins</a> via{' '}
+            <a href={dataParser}>@pomber</a> <br />
+            built with <a href="http://recharts.org/">Recharts</a>,{' '}
+            <a href="https://bloomer.js.org/">Bloomer</a>,{' '}
+            <a href="https://bulma.io">Bulma</a>,{' '}
+            <a href="https://react-select.com">React Select</a> and{' '}
+            <a href="https://reactjs.org">React</a> by{' '}
+            <a href="https://github.com/eminx">Emin Durak</a>
+          </Subtitle>
+        </Container>
+      </Container>
     </div>
   );
 }
---
+++ src/Drawer.jsx
@@ -0,0 +1,457 @@
+import React, { PureComponent } from 'react';
+import { Box, Columns, Heading, Column, Title, Subtitle } from 'bloomer';
+import Select from 'react-select';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+
+const apiEndpoint = 'https://pomber.github.io/covid19/timeseries.json';
+
+const colors = {
+  diagnosed: 'hsla(27, 100%, 51%, 1)',
+  recovered: 'hsla(91, 100%, 51%, 1)',
+  unrecovered: 'hsla(15, 100%, 51%, 1)',
+  died: 'hsla(0, 100%, 51%, 1)',
+};
+
+const dates = ['last 30 days', 'last 7 days', 'since 22nd January'];
+
+class Drawer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      selectedCountry: 'Sri Lanka',
+      loading: true,
+      countries: [],
+      dateIntervals: dates.map((date) => ({ label: date, value: date })),
+      selectedDateInterval: dates[0],
+      width:
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth,
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+    fetch('http://ip-api.com/json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          selectedCountry: data.country,
+        });
+      });
+  }
+
+  getDaysCount = () => {
+    const { selectedDateInterval } = this.state;
+    switch (selectedDateInterval) {
+      case dates[0]:
+        return 30;
+      case dates[1]:
+        return 7;
+      case dates[2]:
+        return 0;
+      default:
+        return 30;
+    }
+  };
+
+  getData = () => {
+    const { selectedCountry } = this.state;
+    if (!selectedCountry) {
+      return;
+    }
+    const daysCount = this.getDaysCount();
+
+    fetch(apiEndpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data) {
+          return null;
+        }
+        const newData = data[selectedCountry]
+          .filter((info, index) => {
+            if (daysCount === 0) {
+              return true;
+            }
+            return index > data[selectedCountry].length - daysCount - 1;
+          })
+          .map(({ date, confirmed, recovered, deaths }) => ({
+            diagnosed: confirmed,
+            recovered: recovered,
+            unrecovered: confirmed - recovered - deaths,
+            died: deaths,
+            date: date.substring(5, date.length),
+          }));
+
+        const countries = Object.keys(data);
+
+        this.setState({
+          data: newData,
+          countries: countries.map((country) => ({
+            label: country,
+            value: country,
+          })),
+          loading: false,
+        });
+      });
+  };
+
+  handleCountrySelect = (country) => {
+    if (!country) {
+      return;
+    }
+
+    this.setState(
+      {
+        selectedCountry: country.value,
+      },
+      () => this.getData()
+    );
+  };
+
+  handleIntervalSelect = (interval) => {
+    this.setState(
+      {
+        selectedDateInterval: interval.value,
+      },
+      () => this.getData()
+    );
+  };
+
+  render() {
+    const {
+      data,
+      countries,
+      selectedCountry,
+      dateIntervals,
+      selectedDateInterval,
+      width,
+    } = this.state;
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          paddingTop: 12,
+          paddingBottom: 24,
+        }}
+      >
+        <div>
+          <Box style={{ marginTop: 12 }}>
+            <Columns>
+              <Column isSize={{ mobile: 12, isDesktop: 4 }}>
+                <Select
+                  isClearable
+                  placeholder={
+                    <b style={{ color: '#3e3e3e' }}>{selectedCountry}</b>
+                  }
+                  value={selectedCountry}
+                  onChange={this.handleCountrySelect}
+                  options={countries}
+                />
+              </Column>
+              <Column isSize={{ mobile: 12, isDesktop: 4 }}>
+                <Select
+                  isSearchable={false}
+                  placeholder={<b>{selectedDateInterval}</b>}
+                  value={selectedDateInterval}
+                  onChange={this.handleIntervalSelect}
+                  options={dateIntervals}
+                />
+              </Column>
+              <Column isSize={{ mobile: 12, isDesktop: 4 }}>
+                <Subtitle isSize={5} hasTextAlign="right">
+                  <em>
+                    Numbers indicate total instances <b>by</b> date, not per
+                    date.
+                  </em>
+                </Subtitle>
+              </Column>
+            </Columns>
+          </Box>
+
+          <Box style={{ padding: 24 }}>
+            <Title isSize={4}>
+              Total Numbers for {selectedCountry} in {selectedDateInterval}
+            </Title>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row-reverse',
+                flexWrap: 'wrap',
+              }}
+            >
+              {Object.keys(colors).map((type, index) => (
+                <LegendCircle key={type} type={type} color={colors[type]} />
+              ))}
+            </div>
+            <AreaChart
+              width={width > 980 ? 960 : width - 48}
+              height={500}
+              data={data}
+              syncId="date"
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="1 3" />
+              <XAxis dataKey="date" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="diagnosed"
+                stroke={colors.diagnosed}
+                fill={colors.diagnosed}
+              />
+              <Area
+                type="monotone"
+                dataKey="unrecovered"
+                stroke={colors.unrecovered}
+                fill={colors.unrecovered}
+              />
+              <Area
+                type="monotone"
+                dataKey="died"
+                stroke={colors.died}
+                fill={colors.died}
+              />
+            </AreaChart>
+          </Box>
+
+          <Box style={{ padding: 24 }}>
+            <Title isSize={4}>
+              Numbers in detail for {selectedCountry}{' '}
+              {selectedDateInterval === dateIntervals[2] ? '' : 'in'}{' '}
+              {selectedDateInterval}
+            </Title>
+
+            <Columns
+              style={{ marginBottom: 48, borderBottom: '1px solid #797979' }}
+            >
+              <Column isSize={{ mobile: 12, isDesktop: 6 }}>
+                <AreaChart
+                  width={
+                    width > 980
+                      ? 480
+                      : width > 720
+                      ? width / 2 - 40
+                      : width - 40
+                  }
+                  height={200}
+                  data={data}
+                  syncId="date"
+                  margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Area
+                    type="monotone"
+                    dataKey="diagnosed"
+                    stroke={colors.diagnosed}
+                    fill={colors.diagnosed}
+                  />
+                </AreaChart>
+              </Column>
+
+              <Column isSize={{ mobile: 6, isDesktop: 3 }}>
+                <Subtitle hasTextAlign="left">
+                  total of{' '}
+                  <Heading style={{ fontSize: 48 }}>
+                    <b>
+                      {data &&
+                        data[data.length - 1] &&
+                        data[data.length - 1].diagnosed}
+                    </b>{' '}
+                  </Heading>
+                  diagnosed
+                </Subtitle>
+              </Column>
+            </Columns>
+
+            <Columns
+              style={{ marginBottom: 48, borderBottom: '1px solid #797979' }}
+            >
+              <Column isSize={{ mobile: 12, isDesktop: 6 }}>
+                <AreaChart
+                  width={
+                    width > 980
+                      ? 480
+                      : width > 720
+                      ? width / 2 - 40
+                      : width - 40
+                  }
+                  height={200}
+                  data={data}
+                  syncId="date"
+                  margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Area
+                    type="monotone"
+                    dataKey="recovered"
+                    stroke={colors.recovered}
+                    fill={colors.recovered}
+                  />
+                </AreaChart>
+              </Column>
+
+              <Column isSize={{ mobile: 6, isDesktop: 3 }}>
+                <Subtitle hasTextAlign="left">
+                  total of{' '}
+                  <Heading style={{ fontSize: 48 }}>
+                    <b>
+                      {data &&
+                        data[data.length - 1] &&
+                        data[data.length - 1].recovered}
+                    </b>{' '}
+                  </Heading>
+                  recovered
+                </Subtitle>
+              </Column>
+
+              <Column isSize={{ mobile: 6, isDesktop: 3 }}>
+                <Subtitle hasTextAlign="left">
+                  with{' '}
+                  <Heading style={{ fontSize: 48 }}>
+                    <b>
+                      {data &&
+                        data[data.length - 1] &&
+                        Math.round(
+                          (data[data.length - 1].recovered /
+                            data[data.length - 1].diagnosed) *
+                            100
+                        )}
+                      %
+                    </b>{' '}
+                  </Heading>
+                  recovery rate
+                </Subtitle>
+              </Column>
+            </Columns>
+
+            <Columns>
+              <Column isSize={{ mobile: 12, isDesktop: 6 }}>
+                <AreaChart
+                  width={
+                    width > 980
+                      ? 480
+                      : width > 720
+                      ? width / 2 - 40
+                      : width - 40
+                  }
+                  height={200}
+                  data={data}
+                  syncId="date"
+                  margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Area
+                    type="monotone"
+                    dataKey="died"
+                    stroke={colors.died}
+                    fill={colors.died}
+                  />
+                </AreaChart>
+              </Column>
+
+              <Column isSize={{ mobile: 6, isDesktop: 3 }}>
+                <Subtitle hasTextAlign="left">
+                  total of{' '}
+                  <Heading style={{ fontSize: 48 }}>
+                    <b>
+                      {data &&
+                        data[data.length - 1] &&
+                        data[data.length - 1].died}
+                    </b>{' '}
+                  </Heading>
+                  died
+                </Subtitle>
+              </Column>
+
+              <Column isSize={{ mobile: 6, isDesktop: 3 }}>
+                <Subtitle hasTextAlign="left">
+                  with{' '}
+                  <Heading style={{ fontSize: 48 }}>
+                    <b>
+                      {data &&
+                        data[data.length - 1] &&
+                        Math.round(
+                          (data[data.length - 1].died /
+                            data[data.length - 1].diagnosed) *
+                            100
+                        )}
+                      %
+                    </b>{' '}
+                  </Heading>
+                  death rate
+                </Subtitle>
+              </Column>
+            </Columns>
+          </Box>
+        </div>
+      </div>
+    );
+  }
+}
+
+const legendCircleStyle = (color) => ({
+  width: 12,
+  height: 12,
+  borderRadius: '50%',
+  backgroundColor: color,
+  marginRight: 4,
+});
+
+function LegendCircle({ type, color }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 24,
+      }}
+    >
+      <div style={legendCircleStyle(color)} />
+      <Heading style={{ marginBottom: 0 }}>{type}</Heading>
+    </div>
+  );
+}
+
+export default Drawer;
